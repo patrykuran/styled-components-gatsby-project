91e262218e81df24127ef634b6375c10b8732c30
Patryk Uran <patryk.uran@dcx.bz>
2019-10-02 17:04:44 +0200

    pass props to style a component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+`
 
 const StyledWrapper = styled.div`
   height: 100vh;
@@ -7,12 +18,25 @@ const StyledWrapper = styled.div`
   justify-content: center;
   align-items: center;
   background: #ffc0cb5e;
-`
+  font-family: Arial;
+  color: #717070;
+`;
+
+const H1 = styled.h1`
+// without destructuring:
+// font-size: ${(props) => props.isBig ? '5em' : '3em'};
+  font-size: ${({isBig}) => isBig ? '5em' : '3em'};
+  display: block;
+`;
 
 const IndexPage = () => (
-  <StyledWrapper>
-    <h1>Hi people</h1>
-  </StyledWrapper>
+  <>
+    <GlobalStyle/>
+    <StyledWrapper>
+      <H1 isBig>Hi people</H1>
+      <H1>Hi people 2</H1>
+    </StyledWrapper>
+  </>
 )
 
 export default IndexPage
